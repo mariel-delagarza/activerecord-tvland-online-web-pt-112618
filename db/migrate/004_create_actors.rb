330d3c82527ba205840dfa7e8dec47b7216f5b31
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-09 22:04:09 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,8 @@
-class Actors < ActiveRecord::Migrate[4.2]
+class CreateActors < ActiveRecord::Migrate[5.1]
   def change
     create_table :actors do |t|
       t.string :first_name
       t.string :last_name
     end
   end
-end 
+end
